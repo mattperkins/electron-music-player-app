92bf23899505e21acae2cefb48025e231b8fcc0b
mattperkins <github@mattperkins.com>
2018-08-15 07:56:01 +0100

    add dev dependencies

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 const electron = require('electron')
 const app = electron.app
-
+const BrowserWindow = electron.BrowserWindow
+let mainWindow = null
+app.on('ready', () => {
+ mainWindow = new BrowserWindow({
+  width: 612,
+  height: 384
+ })
+ mainWindow.loadURL(`file://${__dirname}/app/index.html`)
+})
